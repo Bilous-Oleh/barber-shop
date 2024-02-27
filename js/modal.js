cdf1c5600c87a2b09c41ad34d26aa53540038291
Bilous-Oleh <114952390+Bilous-Oleh@users.noreply.github.com>
2024-02-27 09:25:02 +0000

    Update modal function

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,29 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+//   };
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
+
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtnHeader: document.querySelector(".header-btn"),
+    openModalBtnAbout: document.querySelector(".about-btn"),
+    openModalBtnPrices: document.querySelector(".prices-btn"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
-  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtnHeader.addEventListener("click", toggleModal);
+  refs.openModalBtnAbout.addEventListener("click", toggleModal);
+  refs.openModalBtnPrices.addEventListener("click", toggleModal);
+
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
